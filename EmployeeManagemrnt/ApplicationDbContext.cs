d162bd8fb8ed4bd0a8ea5d579c87f7fc846b5526
Subrat-Kumar-Jalli <jallysk@gmail.com>
2022-12-27 12:53:34 +0530

    DATABASE CONFIGURATION DONE.

---
+++ EmployeeManagemrnt/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EmployeeManagement.Shared;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace EmployeeManagemrnt
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+         //Emp
+            public virtual DbSet<Employee> Employee {get; set;}
+        //Dept
+            public virtual DbSet<Department> Department {get; set;}   
+                  
+
+        
+    }
+}
